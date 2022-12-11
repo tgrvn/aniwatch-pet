8e262a0af26146bc6c124094a5c6edc4a0f5edee
tgrvn <warjllalka@gmail.com>
2022-12-11 13:27:54 +0200

    Filters panel 7/10

---
+++ src/components/searchPanel/SearchPanelAdaptive.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import SearchField from "../ui/searchField/SearchField";
+import SearchSelect from "../ui/searchSelect/SearchSelect";
+import styles from "./SearchPanel.module.css";
+
+export default function SearchPanelAdaptive() {
+  const optionsForGenre = ["Action", "Comedy", "OVA", "Drama"];
+  const optionsForYear = ["2022", "2021", "2020", "2019", "2018"];
+  const optionsForSeasons = ["Winter", "Summer", "Spring", "Fall"];
+  const optionsForFormat = ["TV", "OVA", "Serial", "Film"];
+
+  const [filters, setFilters] = useState({
+    searchQuerry: "",
+    genres: [],
+    year: "",
+    seasons: "",
+    format: "",
+  });
+
+  return (
+    <div className={styles.panel}>
+      <SearchField
+        head={"Search"}
+        value={filters.searchQuerry}
+        setValue={(value) => setFilters({ ...filters, searchQuerry: value })}
+      />
+      <SearchSelect
+        head={"Genres"}
+        options={optionsForGenre}
+        setValue={(value) =>
+          setFilters({ ...filters, genres: [...filters.genres, value] })
+        }
+        removeValue={(value) =>
+          setFilters({
+            ...filters,
+            genres: [...filters.genres].filter((g) => g !== value),
+          })
+        }
+        clear={() => setFilters({ ...filters, genres: [] })}
+        value={filters.genres}
+        search={true}
+        multiply={true}
+      />
+      <SearchSelect
+        head={"Year"}
+        options={optionsForYear}
+        setValue={(value) => setFilters({ ...filters, year: value })}
+        value={filters.year}
+        search={true}
+      />
+      <SearchSelect
+        head={"Seasons"}
+        options={optionsForSeasons}
+        setValue={(value) => setFilters({ ...filters, seasons: value })}
+        value={filters.seasons}
+        disabled={true}
+      />
+      <SearchSelect
+        head={"Format"}
+        options={optionsForFormat}
+        setValue={(value) => setFilters({ ...filters, format: value })}
+        value={filters.format}
+        disabled={true}
+      />
+    </div>
+  );
+}
